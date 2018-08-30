512baba245874b29b87c85c749f55347e5baa16e
Liulab-HZAU <39718514+Liulab-HZAU@users.noreply.github.com>
2018-08-30 19:31:17 +0800

    Update environment_variable.sh

--- bin/environment_variable.sh
+++ bin/environment_variable.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 #========================add environment variable====================
-PCRpred_path=$(readlink -f ..)
+PCTpred_path=$(readlink -f ..)
 
 #domain
-domain_real_path=${PCRpred_path}/software/domain
+domain_real_path=${PCTpred_path}/software/domain
 export PATH=$PATH:${domain_real_path}/hmmer-3.1b2-linux-intel-x86_64/binaries
 export PERL5LIB=${domain_real_path}/PfamScan:$PERL5LIB
 
 #PSAIA
 
-psaia_real_path=${PCRpred_path}/software/PSAIA/PSAIA-1.0
+psaia_real_path=${PCTpred_path}/software/PSAIA/PSAIA-1.0
 LD_LIBRARY_PATH=${psaia_real_path}
 export LD_LIBRARY_PATH
 
